2a4c219d9dd976fc478bd8822e3e4e641b0b901a
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-04-10 18:33:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "your api id"
-api_hash = "your api hash"
-bot_token = "your bot token"
+api_id = "22867163"
+api_hash = "a633197e388c9235b4d0032d921c59dd"
+bot_token = "6352984217:AAFEwHxlcs14SoYCJj3bLhkeN_Cj-GChS8g"
