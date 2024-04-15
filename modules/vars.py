eb609895ef71d21950f8daaec025ece9b7b24066
rihankumar <132763286+rihankumar@users.noreply.github.com>
2024-04-15 19:52:53 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "Your api id"
-api_hash = "Your api hash"
-bot_token = "Your bot token"
+api_id = "29940662"
+api_hash = "b9ccc26a417ce3a998d321086a2edbb6"
+bot_token = "6513648341:AAEm5tbyQPWxU55QzJl2bVOJX6JvA-dy1yU"
